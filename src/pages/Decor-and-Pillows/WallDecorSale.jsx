af8b3cd8c55453cfcca10dfb53f278ac4e315d80
Sourav <souravpramanick1997@gmail.com>
2024-10-10 00:06:36 +0530

    Sorting function added

--- src/pages/Decor-and-Pillows/WallDecorSale.jsx
+++ src/pages/Decor-and-Pillows/WallDecorSale.jsx
@@ -1,16 +1,42 @@
-import { FormLabel, Select } from "@chakra-ui/react";
+import { FormLabel, Select, Spinner } from "@chakra-ui/react";
 import { wallDecorSaleData } from "./data";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import ProductCard from "../../components/ProductCard";
 export default function WallDecorSale() {
   const [sort, setSort] = useState("Recommended");
+  const [data,setData] = useState([...wallDecorSaleData])
+  const [isSorting,setIsSorting] = useState(false)
+  console.log(data[0]);
+  
+  useEffect(()=>{
+    setIsSorting(true)
+    setTimeout(()=>setIsSorting(false),250)
+    if(sort === 'Recommended'){
+      setData([...wallDecorSaleData])
+    }else if(sort === 'Price: Low to High'){
+      let sortedData = [...wallDecorSaleData].sort((a,b)=>a.deal_price - b.deal_price)
+      setData(sortedData)
+    }
+    else if(sort === "Price: High to Low"){
+      let sortedData = [...data].sort((a,b)=>b.deal_price - a.deal_price)
+      setData(sortedData)
+    }
+    else if (sort === ''){
+      let sortedData = [...data].sort((a, b) => {
+        // console.log(a.ratings.split(' ')[1]);
+        
+        +a.ratings.split(' ')[1] - (+b.ratings.split(' ')[1])});
+      setData(sortedData)
+    }
+  },[sort])
+
   return (
     <div className="p-4 w-full ">
       <section id="top" className="flex justify-between p-4">
         <div>
           <p>Décor & Pillows / Wall Décor / Wall Décor Sale</p>
           <h3 className="text-2xl font-bold">Wall Décor Sale</h3>
-          <p>{wallDecorSaleData.length} Results</p>
+          <p>{data.length} Results</p>
         </div>
         <div className="relative">
           <Select
@@ -38,11 +64,19 @@ export default function WallDecorSale() {
           </FormLabel>
         </div>
       </section>
-      <section id="products" className="grid grid-cols-2 min-[960px]:grid-cols-3 gap-6 justify-evenly">
-        {wallDecorSaleData.map((product,index)=>(
+{  isSorting ? <div className="flex items-center m-auto w-fit py-6 gap-8">
+    <h3 className="text-4xl">Loading</h3><Spinner
+  thickness='4px'
+  speed='1s'
+  emptyColor='gray.200'
+  color='blue.500'
+  size='xl'
+/>
+    </div>  :  <section id="products" className="grid grid-cols-2 min-[960px]:grid-cols-3 gap-6 justify-evenly">
+        {data.map((product,index)=>(
           <ProductCard key={index+1} product={product}/>
         ))}
-      </section>
+      </section>}
     </div>
   );
 }
